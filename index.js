4baba6fdf1103ac3b09ca817fbcc27788baa3f8c
Yash Walia <yashbest005@gmail.com>
2020-07-15 17:59:11 +0530

    com2

--- index.js
+++ index.js
@@ -38,3 +38,21 @@ setTimeout(function () {
         $("#loading").css("display", "none");
     }, 800);
 }, 1450);
+
+function magnify(imglink) {
+    $("#img_here").css("background", `url('${imglink}') center center`);
+    $("#magnify").css("display", "flex");
+    $("#magnify").addClass("animated fadeIn");
+    setTimeout(function () {
+        $("#magnify").removeClass("animated fadeIn");
+    }, 800);
+}
+
+function closemagnify() {
+    $("#magnify").addClass("animated fadeOut");
+    setTimeout(function () {
+        $("#magnify").css("display", "none");
+        $("#magnify").removeClass("animated fadeOut");
+        $("#img_here").css("background", `url('') center center`);
+    }, 800);
+}
