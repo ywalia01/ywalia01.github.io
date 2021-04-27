d3e0ce9ca47325f0248bb09a2b4f27e42e9e3238
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-27 22:36:47 +0530

    complete restructure, remove falibrary

---
+++ src/components/About/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './About.css';
+import { ABOUT } from '../../data';
+
+const About = () => {
+    return (
+        <section id="about">
+            <h1>About</h1>
+            <div>
+                {ABOUT.map(para => (
+                    <p>
+                        {para.text}
+                    </p>
+                ))}
+            </div>
+      </section>
+    );
+};
+
+export default About;
