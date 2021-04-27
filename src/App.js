55e985159f644c522073c7d32f65d07d08aef19e
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-27 14:26:28 +0530

    add scrollToTop button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './components/css/App.css';
 import './components/css/fontawesome-free-5.15.3-web/css/all.min.css'
-import Header from "./components/ui/Header";
-import Container from "./components/ui/Container";
-import Footer from "./components/ui/Footer";
+import Header from './components/ui/Header';
+import Container from './components/ui/Container';
+import Footer from './components/ui/Footer';
+import ScrollButton from './components/ui/ScrollButton';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Container />
       <Footer />
+      <ScrollButton />
     </div>
   );
 }
