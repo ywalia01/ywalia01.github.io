1a0d22e817672499160d17c3236326f2a34a929b
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-27 14:27:36 +0530

    add menuIcon color toggle, update styling

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 import menuIcon from "../img/menu.png";
-// import menuIcon2 from "../img/menu2.png";
+import menuIcon2 from "../img/menu2.png";
 
 const Header = () => {
   const [showLinks, setShowLinks] = useState(false);
     return (
       <header>
         <nav>
-          <img src={menuIcon} onClick={()=>setShowLinks(!showLinks)} className="menu-icon" alt="menu-icon"/>
+          <img src={showLinks ? menuIcon2 : menuIcon} onClick={()=>setShowLinks(!showLinks)} className="menu-icon" alt="menu-icon"/>
           <ul className="nav-menu" id={showLinks ? "hidden" : ""} >
             <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
